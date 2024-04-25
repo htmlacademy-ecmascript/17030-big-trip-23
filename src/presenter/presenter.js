4c171b4e760b696d5b824c74bc7927caa1641165
mrnobody <corvus007@yandex.ru>
2024-04-25 23:02:23 +0300

    Создает презентер

---
+++ src/presenter/presenter.js
@@ -0,0 +1,17 @@
+import EventsListView from '../view/events-list-view';
+import EventCreateView from '../view/event-create-view';
+import EventEditView from '../view/event-edit-view';
+import EventItemView from '../view/event-item-view';
+
+export default class Presenter {
+  init() {
+    const tripEventsEl = document.querySelector('.trip-events');
+    const eventsListEl = new EventsListView().getElement();
+
+    eventsListEl.append(new EventCreateView().getElement());
+    eventsListEl.append(new EventEditView().getElement());
+    eventsListEl.append(new EventItemView().getElement());
+
+    tripEventsEl.append(eventsListEl);
+  }
+}
