6147c79dc86948b83a085dae052a8b30608f1849
mrnobody <corvus007@yandex.ru>
2024-04-27 08:00:02 +0300

    Делает рефакторинг кода

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import FiltersView from './view/filters-view';
-import SortingView from './view/sorting-view';
 import Presenter from './presenter/presenter';
 
 const tripControlsFiltersEl = document.querySelector('.trip-controls__filters');
 const tripEventsEl = document.querySelector('.trip-events');
-const presenter = new Presenter();
+const presenter = new Presenter({
+  containerEl: tripEventsEl,
+});
 
 tripControlsFiltersEl.append(new FiltersView().getElement());
-tripEventsEl.append(new SortingView().getElement());
 
 presenter.init();
