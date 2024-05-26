536cadd12628a4b468f2748440a9fca469978cf9
mrnobody <corvus007@yandex.ru>
2024-05-26 10:15:16 +0300

    Использует паттерн наблюдатель для модели точек маршрутов

--- src/model/waypoints-model.js
+++ src/model/waypoints-model.js
@@ -1,8 +1,9 @@
+import Observable from '../framework/observable';
 import { getRandomWaypoint } from '../mock/waypoints';
 
 const WAYPOINTS_COUNT = 4;
 
-export default class WaypointsModel {
+export default class WaypointsModel extends Observable {
   #waypoints = Array.from({ length: WAYPOINTS_COUNT }, getRandomWaypoint);
 
   get waypoints() {
