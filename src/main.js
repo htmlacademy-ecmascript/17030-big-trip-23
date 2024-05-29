bd8319e528c5a3f02835a7260e90ac75e7c03ad6
mrnobody <corvus007@yandex.ru>
2024-05-29 22:07:58 +0300

    Добавит модель для фильтра. WIP

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import TripPresenter from './presenter/trip-presenter';
 import WaypointsModel from './model/waypoints-model';
 import DestinationsModel from './model/destinations-model';
 import OffersModel from './model/offers-model';
+import FilterModel from './model/filter-model';
 import { FilterType } from './const';
 import { generateFilter } from './mock/filter';
 import HeaderPresenter from './presenter/header-presenter';
@@ -12,6 +13,7 @@ const tripEventsEl = document.querySelector('.trip-events');
 const waypointsModel = new WaypointsModel();
 const destinationsModel = new DestinationsModel();
 const offersModel = new OffersModel();
+const filterModel = new FilterModel();
 const activeFilter = FilterType.EVERYTHING;
 const filters = generateFilter(waypointsModel.waypoints);
 
