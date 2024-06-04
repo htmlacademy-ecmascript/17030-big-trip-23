b337f6a7b0d4276e41f7699c96c3cd3a98b1c7b6
mrnobody <corvus007@yandex.ru>
2024-06-04 22:17:48 +0300

    Добавит геттеры для получения пунктов назначения и спец. предложений

--- src/waypoints-api-service.js
+++ src/waypoints-api-service.js
@@ -12,6 +12,14 @@ export default class WaypointsApiService extends ApiService {
     return this._load({ url: 'points' }).then(ApiService.parseResponse);
   }
 
+  get destinations() {
+    return this._load({ url: 'destinations' }).then(ApiService.parseResponse);
+  }
+
+  get offers() {
+    return this._load({ url: 'offers' }).then(ApiService.parseResponse);
+  }
+
   async createWaypoint(waypoint) {
     const response = await this._load({
       url: 'points',
