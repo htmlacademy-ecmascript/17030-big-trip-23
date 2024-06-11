5238d9723bc1fbcc3aab999ac25afbf8d3b07748
mrnobody <corvus007@yandex.ru>
2024-06-11 21:47:56 +0300

    Скроет секцию `Destination` если нет описания и фотографий для нее

--- src/view/waypoint-edit-view.js
+++ src/view/waypoint-edit-view.js
@@ -126,18 +126,14 @@ const createDestinationPhotosTemplate = (picture) => (
   </div>`
 );
 
-const createDestinationDescriptionTemplate = (destination) => {
-  const { description, pictures } = destination;
+const createDestinationDescriptionTemplate = ({ description, pictures }) => (description || pictures?.length) ? (
+  `<section class="event__section  event__section--destination">
+    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+    <p class="event__destination-description">${description}</p>
 
-  return (
-    `<section class="event__section  event__section--destination">
-      <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-      <p class="event__destination-description">${description}</p>
-
-      ${pictures?.length ? createDestinationPhotosTemplate(pictures) : ''}
-    </section>`
-  );
-};
+    ${pictures?.length ? createDestinationPhotosTemplate(pictures) : ''}
+  </section>`
+) : '';
 
 const createOpenEventButtonTemplate = () => (
   `<button class="event__rollup-btn" type="button">
